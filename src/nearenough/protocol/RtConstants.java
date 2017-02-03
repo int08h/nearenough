a91999a4eeea72ec64a6444af953bfe8a3e741fd
Stuart Stock <stuart@int08h.com>
2017-02-03 15:04:31 -0600

    spelling fix

--- src/nearenough/protocol/RtConstants.java
+++ src/nearenough/protocol/RtConstants.java
@@ -8,7 +8,7 @@ public final class RtConstants {
   /** Minimum size (in bytes) of a client request */
   public static final int MIN_REQUEST_LENGTH = 1024;
 
-  /** Minimum size (in bytes) of seeds used to derived private keys */
+  /** Minimum size (in bytes) of seeds used to derive private keys */
   public static final int MIN_SEED_LENGTH = 32;
 
   /** Size (in bytes) of an Ed25519 public key */
