45ef37998e5cc8048b4ff5c7747a0eee117ebe2a
Stuart Stock <stuart@int08h.com>
2017-02-06 13:16:57 -0600

    Update copyright on all files

--- src/nearenough/util/Preconditions.java
+++ src/nearenough/util/Preconditions.java
@@ -1,20 +1,19 @@
-package nearenough.util;
-
 /*
- * Derived from Guava 21.0
+ * Copyright (c) 2017 int08h LLC. All rights reserved.
  *
- * Copyright (C) 2007 The Guava Authors
+ * int08h LLC licenses Nearenough (the "Software") to you under the Apache License, version 2.0
+ * (the "License"); you may not use this Software except in compliance with the License. You may
+ * obtain a copy of the License from the LICENSE file included with the Software or at:
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
+package nearenough.util;
 
 /**
  * Static convenience methods that help a method or constructor check whether it was invoked
